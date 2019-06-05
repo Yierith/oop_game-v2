9ac2fb8b60fb3d8e0999acc86a0a75126f035e6e
Tim Rach <tim.rach1987@gmail.com>
2019-06-05 11:52:28 +0200

    Added addPhraseToDisplay Func

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,4 +6,29 @@ class Phrase {
   constructor(phrase) {
     this.phrase = phrase.toLowerCase();
   }
+
+  /**
+  * Display phrase on game board
+  */
+  addPhraseToDisplay(){
+    let phraseUl = document.getElementsByTagName('UL')[0];
+    for( let i = 0; i < this.phrase.length; i++ ){
+      let li = document.createElement('li');
+      if( this.phrase[i] != " "){
+        li.className = `hide letter ${this.phrase[i]}`;
+      }else{
+        li.className = `hide space`;
+      }
+      li.appendChild(document.createTextNode(this.phrase[i]))
+      phraseUl.appendChild(li);
+    }
+  };
+
+
+
+
+
+
+
+
 }
