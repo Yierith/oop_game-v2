3acb0a9f5898170c35f034f6e361930c18fcf3fa
Tim Rach <tim.rach1987@gmail.com>
2019-06-05 11:52:07 +0200

    Created Game Class createPhrase Func getRandomPhrase Func

--- js/Game.js
+++ js/Game.js
@@ -5,7 +5,32 @@
 class Game {
   constructor() {
     this.missed = 0;
-    this.phrases = [];
+    this.phrases = this.createPhrases();
     this.activePhrase = null;
   }
+
+  /**
+  * Creates phrases for use in game
+  * @return {array} An array of phrases that could be used in the game
+  */
+  createPhrases(){
+    return [new Phrase("You can totally do this"),
+            new Phrase("Stay foolish to stay sane"),
+            new Phrase("I can and I will"),
+            new Phrase("Take the risk or lose the chance"),
+            new Phrase("Prove them wrong")];
+  };
+
+  /**
+  * Selects random phrase from phrases property
+  * @return {Object} Phrase object chosen to be used
+  */
+  getRandomPhrase(){
+    return this.phrases[Math.floor(Math.random()*this.phrases.length)];
+  };
+
+
+
+
+
 }
