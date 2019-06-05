f554143f9875f44b2a4550b0959e8415b01bd1d4
Tim Rach <tim.rach1987@gmail.com>
2019-06-05 10:00:47 +0200

    testing

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+
+const phrase = new Phrase('Life is like a box of chocolates');
+console.log(`Phrase - phrase: ${phrase.phrase}`);
+
+
+
+
+
+
+// Step 4
