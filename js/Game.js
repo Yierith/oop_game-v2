8c9af5f2f588ba231af51bb2f64197f501a58545
Tim Rach <tim.rach1987@gmail.com>
2019-06-18 13:44:55 +0200

    Commenting

--- js/Game.js
+++ js/Game.js
@@ -34,14 +34,17 @@ class Game {
   */
   gameReset(){
     let phraseUl = document.getElementsByTagName('UL')[0].innerHTML = '';
-    let allb = document.getElementsByTagName('button');
-    for(let i = 0; i < allb.length; i++){
-      allb[i].className = 'key';
+    let allButtons = document.getElementsByTagName('button');
+    // Iterting over allbuttons from the keyboard ul and changing
+    // their class to 'key'
+    for(let i = 0; i < allButtons.length; i++){
+      allButtons[i].className = 'key';
     }
     const heartImages = document.querySelectorAll('.tries');
     for(let i = 0; i < heartImages.length; i++){
       heartImages[i].firstChild.src = 'images/liveHeart.png';
     }
+    // resetting the missed counter
     this.missed = 0;
 
   }
@@ -52,8 +55,11 @@ class Game {
   */
   startGame(){
     game.gameReset();
+    // Hiding the overlay
     document.getElementById('overlay').style.display = 'none';
+    // Getting a randomPhrase
     this.activePhrase = this.getRandomPhrase();
+    // Adding it to the display
     this.activePhrase.addPhraseToDisplay();
   };
 
@@ -62,6 +68,7 @@ class Game {
   * @return {boolean} True if game has been won, false if game wasn't won
   */
   checkForWin(){
+    // Get all LI with the class letter
     const letterLi = document.querySelectorAll(`.hide.letter`);
     if( letterLi.length > 0  ){
       return false;
@@ -76,14 +83,16 @@ class Game {
   * Checks if player has remaining lives and ends game if player is out
   */
   removeLife(){
-    const heartOL = document.querySelector('#scoreboard ol');
     const heartImages = document.querySelectorAll('.tries');
     const firstImage = heartImages[this.missed];
+    // Changing color of the body when player is guessing wrong
+    // for 300 ms and changing it back
     document.querySelector('body').style.backgroundColor = 'coral';
     setTimeout(function(){
-    //do what you need here
       document.querySelector('body').style.backgroundColor = '';
     }, 300);
+    // Miss counter -> if miss lower than 4 add a miss
+    // If not game is lost.
     if (this.missed < 4){
       heartImages[this.missed].firstChild.src = 'images/lostHeart.png';
       this.missed = this.missed + 1;
@@ -99,9 +108,12 @@ class Game {
   * @param {boolean} gameWon - Whether or not the user won the game
   */
   gameOver(gameWon){
+    // Creating the gameover overlay
     const overlay = document.querySelector('#overlay');
     const overlayH1 = document.querySelector('#game-over-message');
+    // Showing the overlay.
     overlay.style.display = '';
+    // Showing different messages and colors if the game is lost or won
     if(gameWon){
       overlayH1.textContent = 'Great job!';
       overlay.style.backgroundColor = '#5ac179';
@@ -116,21 +128,27 @@ class Game {
   * @param (HTMLButtonElement) button - The clicked button element
   */
   handleInteraction(event){
+    // Getting all keys
     const keys = document.getElementsByClassName('key');
     let letter = event.key;
     let targetKey;
+    // Getting the pressed key out of all keys
     for(let key of keys){
       if( key.textContent === event.key){
         targetKey = key;
       }
     }
+    // disabling the key
     targetKey.setAttribute('disabled', 'disabled')
+    // Adding the class chosen to each key which is in activePhrase
     if ( this.activePhrase.checkLetter(letter) ){
       targetKey.classList.add('chosen');
       this.activePhrase.showMatchedLetter(letter)
+      // Checking everytime if all letters of the Phrase are revealed and game is won or not
       if(this.checkForWin()){
         this.gameOver(true);
       }
+    // Adding the class wrong to each key pressed which is not in activePhrase
     }else{
       targetKey.classList.add('wrong');
       this.removeLife();
